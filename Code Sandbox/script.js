2f9d027e5fd78b09309fcb245c44385b385d9838
alishansnsn <alishansnsn@users.noreply.github.com>
2022-11-17 00:00:00 +0000

    refactor: simplify function logic

--- Code Sandbox/script.js
+++ Code Sandbox/script.js
@@ -123,4 +123,5 @@ const modalMessage = document.getElementById('modal-message');
 const modalCloseButton = document.getElementById('modal-close-button');
 
 
-const defaultHtml = `<h1>Hello, Sandbox!</h1>\n<p>Start coding here.</p>`;
+const defaultHtml = `<h1>Hello, Sandbox!</h1>\n<p>Start coding here.</p>`;
+const defaultCss = `body {\n  font-family: sans-serif;\n  color: #333;\n}`;
