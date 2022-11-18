93685828f707ef94c938073589b48bff417c33b2
alishansnsn <alishansnsn@users.noreply.github.com>
2022-11-18 00:00:00 +0000

    style: adjust formatting and spacing

--- Code Sandbox/script.js
+++ Code Sandbox/script.js
@@ -124,4 +124,5 @@ const modalCloseButton = document.getElementById('modal-close-button');
 
 
 const defaultHtml = `<h1>Hello, Sandbox!</h1>\n<p>Start coding here.</p>`;
-const defaultCss = `body {\n  font-family: sans-serif;\n  color: #333;\n}`;
+const defaultCss = `body {\n  font-family: sans-serif;\n  color: #333;\n}`;
+const defaultJs = `console.log("Welcome to the Code Sandbox!");`;
