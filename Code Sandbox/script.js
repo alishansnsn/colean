77246aa28458c49c7781892370cfbabfef05cb9b
alishansnsn <alishansnsn@users.noreply.github.com>
2022-12-07 00:00:00 +0000

    refactor: split code into smaller functions

--- Code Sandbox/script.js
+++ Code Sandbox/script.js
@@ -164,4 +164,5 @@ function saveCode() {
 
 function loadCode() {
     const savedHtml = localStorage.getItem('sandboxHtml');
-    const savedCss = localStorage.getItem('sandboxCss');
+    const savedCss = localStorage.getItem('sandboxCss');
+    const savedJs = localStorage.getItem('sandboxJs');
