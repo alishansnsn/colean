df90cce8852d5383ade78e5591719dc93b721051
alishansnsn <alishansnsn@users.noreply.github.com>
2023-05-26 00:00:00 +0000

    update project files and improvements

--- fullstackproject/public/js/main.js
+++ fullstackproject/public/js/main.js
@@ -85,4 +85,5 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     }); // updated
 });
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', () => {
+    const dataInput = document.getElementById('dataInput');
