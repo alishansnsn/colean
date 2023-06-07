91bb7d043a6aa91302e0525df7fe5e4266cae1db
alishansnsn <alishansnsn@users.noreply.github.com>
2023-06-07 00:00:00 +0000

    update dependencies and config files

--- fullstackproject/public/js/main.js
+++ fullstackproject/public/js/main.js
@@ -111,3 +111,4 @@ document.addEventListener('DOMContentLoaded', () => {
         } catch (error) {
             serverMessage.textContent = 'Error: Could not connect to the server.'; // updated
             console.error('Error:', error); // updated
+        } // updated
