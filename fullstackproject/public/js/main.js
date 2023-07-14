9c93d9849182ed5d394777bce27999e57d3961ad
alishansnsn <alishansnsn@users.noreply.github.com>
2023-07-14 00:00:00 +0000

    refactor: minor code structure improvements

--- fullstackproject/public/js/main.js
+++ fullstackproject/public/js/main.js
@@ -84,7 +84,7 @@ document.addEventListener('DOMContentLoaded', () => {
             console.error('Error:', error); // updated
         }
     }); // updated
-});
+}); // updated
 document.addEventListener('DOMContentLoaded', () => {
     const dataInput = document.getElementById('dataInput');
     const sendBtn = document.getElementById('sendBtn'); // updated // updated
