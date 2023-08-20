91533003a9008edf53f0ff59607334037c55f997
alishansnsn <alishansnsn@users.noreply.github.com>
2023-08-20 00:00:00 +0000

    improve project folder organization

--- fullstackproject/public/js/main.js
+++ fullstackproject/public/js/main.js
@@ -13,7 +13,7 @@ document.addEventListener('DOMContentLoaded', () => {
         try {
             const response = await fetch('/api/data', {
                 method: 'POST',
-                headers: {
+                headers: { // updated
                     'Content-Type': 'application/json',
                 },
                 body: JSON.stringify({ message: dataToSend }), // updated
