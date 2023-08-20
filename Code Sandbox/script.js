2595b61fb76f863e94422d814de52011543f598a
alishansnsn <alishansnsn@users.noreply.github.com>
2023-08-20 00:00:00 +0000

    update imports and exports in modules

--- Code Sandbox/script.js
+++ Code Sandbox/script.js
@@ -66,7 +66,7 @@ function loadCode() {
 
 // Function to reset all the code to its default state
 function resetCode() { // updated
-    const isConfirmed = window.confirm("Are you sure you want to reset all code? This action cannot be undone.");
+    const isConfirmed = window.confirm("Are you sure you want to reset all code? This action cannot be undone."); // updated
     if (isConfirmed) {
         htmlCode.value = defaultHtml;
         cssCode.value = defaultCss;
