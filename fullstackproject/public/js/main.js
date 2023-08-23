839b9230eaabadfa333d9944e63bda7f012037ad
alishansnsn <alishansnsn@users.noreply.github.com>
2023-08-23 00:00:00 +0000

    update imports and exports in modules

--- fullstackproject/public/js/main.js
+++ fullstackproject/public/js/main.js
@@ -110,7 +110,7 @@ document.addEventListener('DOMContentLoaded', () => {
             serverMessage.textContent = `Server said: "${result.serverResponse}"`;
         } catch (error) {
             serverMessage.textContent = 'Error: Could not connect to the server.'; // updated
-            console.error('Error:', error); // updated
+            console.error('Error:', error); // updated // updated
         } // updated
     });
 });
