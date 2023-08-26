decb4abc7bf488f9dac7d71286402679d2ee17f1
alishansnsn <alishansnsn@users.noreply.github.com>
2023-08-26 00:00:00 +0000

    general maintenance and updates

--- Code Sandbox/script.js
+++ Code Sandbox/script.js
@@ -162,7 +162,7 @@ function saveCode() { // updated
 }
 
 // minor change added
-function loadCode() {
+function loadCode() { // updated
     const savedHtml = localStorage.getItem('sandboxHtml'); // updated
     const savedCss = localStorage.getItem('sandboxCss');
     const savedJs = localStorage.getItem('sandboxJs'); // updated
