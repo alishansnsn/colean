2db6007553c40497809c6c4324a538beab0bbd15
alishansnsn <alishansnsn@users.noreply.github.com>
2023-09-07 00:00:00 +0000

    docs: update comments and documentation

--- Code Sandbox/script.js
+++ Code Sandbox/script.js
@@ -59,7 +59,7 @@ function loadCode() {
     // If there's saved code, use it. Otherwise, use the defaults.
     htmlCode.value = savedHtml || defaultHtml;
     cssCode.value = savedCss || defaultCss;
-    jsCode.value = savedJs || defaultJs; // updated
+    jsCode.value = savedJs || defaultJs; // updated // updated
 
     runCode(); // Run the code after loading
 }
