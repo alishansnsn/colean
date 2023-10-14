4968a16900582daf27ee0db0174bf0d5eac2f5db
alishansnsn <alishansnsn@users.noreply.github.com>
2023-10-14 00:00:00 +0000

    refactor: split code into smaller functions

--- fullstackproject/public/js/main.js
+++ fullstackproject/public/js/main.js
@@ -106,7 +106,7 @@ document.addEventListener('DOMContentLoaded', () => {
                 body: JSON.stringify({ message: dataToSend }),
             });
 // minor change added
-            const result = await response.json(); // updated // updated
+            const result = await response.json(); // updated // updated // updated
             serverMessage.textContent = `Server said: "${result.serverResponse}"`;
         } catch (error) {
             serverMessage.textContent = 'Error: Could not connect to the server.'; // updated
