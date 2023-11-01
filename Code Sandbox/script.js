4cc3d9891af980f27d9cf4f28a0e938401b80c3d
alishansnsn <alishansnsn@users.noreply.github.com>
2023-11-01 00:00:00 +0000

    improve error handling for better stability

--- Code Sandbox/script.js
+++ Code Sandbox/script.js
@@ -216,6 +216,6 @@ modalCloseButton.addEventListener('click', () => { // updated
     notificationModal.classList.add('hidden');
 });
 // minor change added
-// --- Initialization ---
+// --- Initialization --- // updated
 // Load the code from local storage on page load
 window.onload = loadCode;
