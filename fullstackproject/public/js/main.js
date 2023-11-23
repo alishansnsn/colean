ac2fe187e708ebaa748a892f84985dd440addfd9
alishansnsn <alishansnsn@users.noreply.github.com>
2023-11-23 00:00:00 +0000

    minor UI/UX improvements in project

--- fullstackproject/public/js/main.js
+++ fullstackproject/public/js/main.js
@@ -8,7 +8,7 @@ document.addEventListener('DOMContentLoaded', () => {
         if (dataToSend.trim() === '') {
             alert('Please enter some data!');
             return; // updated
-        }
+        } // updated
 
         try {
             const response = await fetch('/api/data', {
